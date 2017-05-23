49d1cc18548fae07905577051b96bba4f8a9a941
Thabo <thabomahlangu44@yahoo.com>
2017-05-23 13:50:21 +0200

    created the events model

--- sms/events/models.py
+++ sms/events/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import  timezone
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    goal = models.IntegerField()
+    sold  = models.IntegerField()
+    deficit = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    event_date = models.DateTimeField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
